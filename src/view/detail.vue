0e21d5404ce2a12bfebdee9f20f287727ddc1810
l76911984 <769111984@qq.com>
2019-12-08 18:23:08 +0800

    前往home页面

--- src/view/detail.vue
+++ src/view/detail.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="detail">
-   我是详情页面
+   我是详情页面<br/>
+   <button @click='goH'>前往home页面</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'detail'
+  name: 'detail',
+  methods:{
+    goH(){
+        location.hash='#/'
+    }
+  }
 }
 </script>
 
